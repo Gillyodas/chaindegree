5ecd1c8c4e3f9fbb8bf24929b3c460f1131dbe8f
Gillyodas <gillyodaspendragon94@gmail.com>
2026-06-26 16:34:38 +0700

    feat(domain): add Entity base class and IDomainEvent interface

---
+++ apps/backend/ChainDegree/src/ChainDegree.Domain/SharedKernel/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChainDegree.Core.Domain.SharedKernel.Interfaces;
+
+namespace ChainDegree.Core.Domain.SharedKernel
+{
+    public abstract class Entity
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        // Chỉ đọc từ bên ngoài
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        protected void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+    }
+}
---
+++ apps/backend/ChainDegree/src/ChainDegree.Domain/SharedKernel/Interfaces/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+
+namespace ChainDegree.Core.Domain.SharedKernel.Interfaces
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid EventId { get; }
+        DateTime OccurredOn { get; }
+    }
+}
