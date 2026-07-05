d97799028b30e01dc5d0f52e3b6ab1b4a3901df8
Gillyodas <gillyodaspendragon94@gmail.com>
2026-07-05 15:40:51 +0700

    feat: implement IProblemException on RepositoryException and RepositoryConcurrencyException

--- apps/backend/ChainDegree/src/ChainDegree.Application/Common/Exceptions/RepositoryConcurrencyException.cs
+++ apps/backend/ChainDegree/src/ChainDegree.Application/Common/Exceptions/RepositoryConcurrencyException.cs
@@ -4,6 +4,9 @@ namespace ChainDegree.Core.Application.Common.Exceptions
 {
     public class RepositoryConcurrencyException : RepositoryException
     {
+        public override int StatusCode => 409;
+        public override string ErrorCode => "CONCURRENCY_ERROR";
+
         public RepositoryConcurrencyException(string message) : base(message) { }
         public RepositoryConcurrencyException(string message, Exception innerException) : base(message, innerException) { }
     }
--- apps/backend/ChainDegree/src/ChainDegree.Application/Common/Exceptions/RepositoryException.cs
+++ apps/backend/ChainDegree/src/ChainDegree.Application/Common/Exceptions/RepositoryException.cs
@@ -2,8 +2,12 @@ using System;
 
 namespace ChainDegree.Core.Application.Common.Exceptions
 {
-    public class RepositoryException : Exception
+    public class RepositoryException : Exception, IProblemException
     {
+        public virtual int StatusCode => 400;
+        public virtual string ErrorCode => "REPOSITORY_ERROR";
+        public virtual string Detail => Message;
+
         public RepositoryException(string message) : base(message) { }
         public RepositoryException(string message, Exception innerException) : base(message, innerException) { }
     }
